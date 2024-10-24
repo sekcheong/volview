4b189276035cd83ab045fb61c0079628e567c610
Sek Cheong <scheong6@its.jnj.com>
2024-10-24 12:05:31 -0500

    add react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <h2>💖 Hello From React</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
