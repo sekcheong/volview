9b56088bfd493d3dedaf7550ecc9b80f13157b2d
Sek Cheong <scheong6@its.jnj.com>
2024-10-24 13:50:34 -0500

    add bootstrap

--- webpack.rules.ts
+++ webpack.rules.ts
@@ -38,4 +38,28 @@ export const rules: Required<ModuleOptions>['rules'] = [
       }
     }
   },
+  {
+    test: /\.(scss)$/,
+    use: [
+      {
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: () => [
+              require('autoprefixer')
+            ]
+          }
+        }
+      },
+      {
+        loader: 'sass-loader'
+      }
+    ]
+  },
 ];
